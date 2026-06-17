e2d75208dedb0eb6b4f17743be37bd4113667e3b
Miguel Angel <mortizdelburgo@gmail.com>
2026-06-17 23:15:40 +0200

    Remove embedded libft

--- libft/srcs/ft_is_exact_word.c
+++
@@ -1,9 +0,0 @@
-
-#include "../lib/libft.h"
-
-int ft_is_exact_word(char *str, char *cmp)
-{
-	if (ft_strncmp(str,cmp,ft_strlen(cmp)) == 0 && ft_strlen(str) == ft_strlen(cmp))
-		return 1;
-	return 0;
-}
